a2796cd21ee0502991a78fbca6278bcdb8157e30
vonhac.20394 <vonhac.20394@gmail.com>
2024-08-04 15:32:15 +0700

    chore: commit

---
+++ index.tsx
@@ -0,0 +1,5 @@
+import WheelPicker from './src/WheelPicker'
+import TimePicker from './src/TimePicker'
+import DatePicker from './src/DatePicker'
+
+export { WheelPicker, TimePicker, DatePicker }
---
+++ src/WheelPicker.ios.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { View } from "react-native";
+import { Picker } from "@react-native-community/picker";
+
+interface Props {
+  data: Array<string>;
+  selectedItem?: number;
+  onItemSelected?: Function;
+  disabled?: boolean;
+}
+
+const WheelPicker: React.FC<Props> = props => {
+  const [selectedItem, setSelectedItem] = useState(props.selectedItem || 0);
+  const { data, onItemSelected, disabled } = props;
+  if (!data || data.length === 0) return null;
+  return (
+    <View pointerEvents={disabled ? "none" : "auto"}>
+      <Picker
+        {...props}
+        selectedValue={data[selectedItem]}
+        onValueChange={(value, index): void => {
+          if (onItemSelected) onItemSelected(index);
+          setSelectedItem(index);
+        }}
+      >
+        {data.map((i, index) => (
+          <Picker.Item key={index} label={i} value={i} />
+        ))}
+      </Picker>
+    </View>
+  );
+};
+
+export default WheelPicker;
